4a30915becfb20d19059426a08aca3cf74d53011
JDH-dev-993 <abobcha8@gmail.com>
2025-09-03 15:13:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('Сайт запущен!');
+console.log('The site was created with the support of Murka Technologies');
+function copyText(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => showNotification())
+    .catch(err => console.error("Ошибка копирования: ", err));
+}
+function showNotification() {
+  const notification = document.getElementById("copyNotification");
+  notification.style.display = "block";
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 2500);
+}
+
+function err() {
+alert("Данная страница в процессе разработки! Вы не можете на неё зайти")
+}
+
+function showBlock() {
+      document.getElementById('overlay').style.display = 'flex';
+}
+
+function hideBlock() {
+      document.getElementById('overlay').style.display = 'none';
+}
